15241f76fdf6340c1212d43378a50042e4e7f704
Rex1o <matlavriv@gmail.com>
2023-06-29 13:46:50 -0400

    Added view

--- initdb/03-views.sql
+++ initdb/03-views.sql
@@ -14,3 +14,15 @@ CREATE VIEW CoursMembre AS
     JOIN role r on grmem.id_role = r.id_role
     JOIN nogroup nogr on gr.id_nogroup = nogr.id_nogroup
     JOIN assignment a on gr.id_group = a.id_group;
+    
+    
+CREATE VIEW SessionRole as
+SELECT s.name as SessionNom, r.name as RoleName, gm.cip as Cip
+FROM SESSION AS S
+INNER JOIN sessionclass sc on S.id_session = sc.id_session
+INNER JOIN class c on c.id_class = sc.id_class
+INNER JOIN groupe as g ON c.id_class = g.id_class
+INNER JOIN groupmember gm on g.id_group = gm.id_group
+INNER JOIN role r on gm.id_role = r.id_role
+group by s.name, r.name, gm.cip
+
